99ae9f6350c2e857d7ab301a0cc52bcc34d39706
Nikolay Postnikov <nick.postnikov@gmail.com>
2020-08-22 11:43:08 +0300

    Maximum coverage

--- src/__tests__/jsvrx.test.ts
+++ src/__tests__/jsvrx.test.ts
@@ -1,16 +1,15 @@
 /* no tsconfig.json at design time available (default is used?), eslint is ok */
-import { of, from, concat } from 'rxjs';
+import { from, concat, EMPTY } from 'rxjs';
 import { reduce, toArray, tap, map, catchError, mergeMap } from 'rxjs/operators';
 
-//import {} from '../../lib/jsvrx';
-import { AjvDataValidator } from '../../lib/jsvrx-ajv';
-
 import { JSONSchema7 } from 'json-schema';
-import { getSchemaID } from '../../lib/jsvrx';
 
-type JSONSchema7$id = JSONSchema7 & { $id: string };
+import { getSchemaID, ValidationError } from '../jsvrx';
+import { AjvDataValidator } from '../jsvrx-ajv';
 
-const js1: JSONSchema7$id = {
+import ajv from 'ajv';
+
+const js1: JSONSchema7 = {
   $id: 'SCHEMA1',
   type: 'object',
   properties: {
@@ -23,7 +22,7 @@ type T1 = {
 };
 type Mapping1 = { SCHEMA1: T1 };
 
-const js2: JSONSchema7$id = {
+const js2: JSONSchema7 = {
   $id: 'SCHEMA2',
   type: 'object',
   properties: {
@@ -38,26 +37,36 @@ type Mapping2 = { SCHEMA2: T2 };
 
 const sample1: T1[] = [{ value: 0 }, { value: 1 }, { value: 2 }];
 const sample2: T2[] = [{ data: 'H' }, { data: 'R' }];
+const sample = [sample1[0], sample2[0], sample1[1], sample1[2], sample2[1]];
 type JSMap = Mapping1 & Mapping2;
 
 test('Validator test (Ajv)', (done) => {
-  const dv = new AjvDataValidator();
+  expect.assertions(3);
+  const dv = new AjvDataValidator(new ajv());
   dv.addSchemas([js1]);
+  expect(() => dv.validator('SCHEMA2')).toThrowError(new RangeError('Unknown schema: SCHEMA2.'));
   from(sample1)
     .pipe(
       dv.validator('SCHEMA1'),
       toArray(),
-      tap((result) => expect(result).toStrictEqual(sample1))
+      tap((result) => expect(result).toStrictEqual(sample1)),
+      dv.validator('SCHEMA1'),
+      catchError((e) => {
+        expect(e).toBeInstanceOf(ValidationError);
+        return EMPTY;
+      })
     )
     .subscribe({ complete: () => void done() });
 });
 
 test('Discriminator test (Ajv)', (done) => {
+  expect.assertions(6);
   const dv = new AjvDataValidator();
   dv.addSchemas([js1, js2]);
-  from([sample1[0], sample2[0], sample1[1], sample1[2], sample2[1]])
-    .pipe(
-      dv.discriminator(['SCHEMA1', 'SCHEMA2']),
+  expect(() => dv.discriminator(['SCHEMA2', 'SCHEMA3'])).toThrowError(new RangeError('Unknown schemas: SCHEMA3.'));
+  concat(
+    from(sample).pipe(
+      dv.discriminator<JSMap, false>(['SCHEMA1', 'SCHEMA2']),
       mergeMap((gobs) =>
         gobs.pipe(
           toArray(),
@@ -69,6 +78,39 @@ test('Discriminator test (Ajv)', (done) => {
         expect(result['SCHEMA1']).toStrictEqual(sample1);
         expect(result['SCHEMA2']).toStrictEqual(sample2);
       })
+    ),
+    from(sample).pipe(
+      dv.discriminator(['SCHEMA1'], 'UNK'),
+      mergeMap((gobs) =>
+        gobs.pipe(
+          toArray(),
+          map((v) => ({ [gobs.key]: v }))
+        )
+      ),
+      reduce((acc, v) => ({ ...acc, ...v }), {} as Record<string, unknown>),
+      tap((result) => {
+        expect(result['SCHEMA1']).toStrictEqual(sample1);
+        expect(result['UNK']).toStrictEqual(sample2);
+      })
+    ),
+    from(sample).pipe(
+      dv.discriminator(['SCHEMA1']),
+      catchError((e) => {
+        expect(e).toBeInstanceOf(ValidationError);
+        return EMPTY;
+      })
     )
-    .subscribe({ complete: () => void done() });
+  ).subscribe({ complete: () => void done() });
+});
+
+test('Helpers test', (done) => {
+  expect(getSchemaID({ $id: 'id' })).toEqual('id');
+  expect(getSchemaID({ id: 'id' })).toEqual('id');
+  expect(getSchemaID({ $id: 'id', id: 'id4' })).toEqual('id');
+  expect(getSchemaID({})).toBeUndefined();
+  expect(getSchemaID({ $id: 'id' }, '$id')).toEqual('id');
+  expect(getSchemaID({ $id: 'id' }, 'id')).toBeUndefined();
+  expect(getSchemaID({ id: 'id' }, '$id')).toBeUndefined();
+  expect(getSchemaID({ id: 'id' }, 'id')).toEqual('id');
+  done();
 });
